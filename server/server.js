7e62501f7ebffd6815a1168a996948afa4501eb5
Zack Stempowicz <zackstempowicz@gmail.com>
2025-08-16 19:46:50 -0400

    changed origin

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-  origin: "https://sports-roadtrip-finder.netlify.app/"
+  origin: "https://sports-roadtrip-finder.netlify.app"
 }));
 
 const PORT = process.env.PORT || 8888;
